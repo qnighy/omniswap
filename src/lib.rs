f685af064d1e3cca4df7d0f034503006a8058fd9
Masaki Hara <ackie.h.gmai@gmail.com>
2022-08-20 16:36:31 +0900

    Implement Replace

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ macro_rules! take {
         $crate::TakeHelper::new($p).take()
     };
     ($p: expr,) => {
-        take!($p)
+        $crate::take!($p)
     };
 }
 
@@ -127,5 +127,28 @@ where
     pub fn take(&mut self) -> T {
         let r = self.0.borrow();
         r.clone()
+    }
+}
+
+pub trait Replace<T> {
+    fn replace(self, value: T) -> T;
+}
+
+impl<'a, T> Replace<T> for &'a mut T {
+    fn replace(self, value: T) -> T {
+        mem::replace(self, value)
+    }
+}
+
+impl<'a, T> Replace<T> for &'a Cell<T> {
+    fn replace(self, value: T) -> T {
+        self.replace(value)
+    }
+}
+
+impl<'a, T> Replace<T> for &'a RefCell<T> {
+    fn replace(self, value: T) -> T {
+        let mut r = self.borrow_mut();
+        mem::replace(&mut *r, value)
     }
 }
